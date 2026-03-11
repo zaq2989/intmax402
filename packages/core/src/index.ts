746025a236653446818bf8f5f94c3c6ab024d2c1
zaq2989 <zaq2989@users.noreply.github.com>
2026-03-11 16:25:39 +0900

    feat: add structured error codes (INTMAX402Error) to core and express

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,4 @@ export { generateNonce } from "./nonce";
 export { verifyNonce } from "./verify";
 export { parseWWWAuthenticate, parseAuthorization } from "./parse";
 export { buildWWWAuthenticate } from "./www-authenticate";
+export { INTMAX402Error, INTMAX402_ERROR_CODES, type INTMAX402ErrorCode } from "./errors";
